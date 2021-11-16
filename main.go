d0b5ed7aa428c3c31de15798060eefe88eb57ff4
Michal <mpjurgo@gmail.com>
2021-11-16 18:43:33 +0100

    Add binsearch avg time

--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"algorytmy/proj1"
-	"fmt"
-	"time"
 )
 
 func main() {
-	fmt.Println("test")
-	arryLen := proj1.Power(2, 28)
-	start := time.Now()
+	arryLen := proj1.Power(2, 25)
 	arry := proj1.PopulateArray(arryLen)
-	proj1.TimeDiff(start, "Populating array")
-	start = time.Now()
-	fmt.Println(proj1.BinSearch(arry, 214542))
-	proj1.TimeDiff(start, "Binary search")
+
+	// proj1.BinarySearchMax(arry)
+	// proj1.BinarySearchMin(arry)
+	// proj1.BinarySearchAvg(arry)
+	proj1.BinarySearchAvgTime(arry)
 }
--- proj1/helpers.go
+++ proj1/helpers.go
@@ -24,5 +24,54 @@ func PopulateArray(length int) []int {
 func TimeDiff(start time.Time, name string) {
 	// elapsed := time.Since(start).Seconds()
 	elapsed := time.Since(start)
-	fmt.Printf("%s took %s.\n", name, elapsed)
+	fmt.Printf("%s wynosi %s.\n", name, elapsed)
+}
+
+func BinarySearchMax(arry []int) {
+	start := time.Now()
+	_, operationCount := BinSearch(arry, 0)
+	TimeDiff(start, "Czas wykonania w przypadku pesymistycznym")
+	fmt.Printf("Liczba operacji w przypadku pesymistycznym: %d\n", operationCount)
+}
+
+func BinarySearchMin(arry []int) {
+	start := time.Now()
+	_, operationCount := BinSearch(arry, len(arry)/2-1)
+	TimeDiff(start, "Czas wykonania w przypadku optymistycznym")
+	fmt.Printf("Liczba operacji w przypadku optymistycznym: %d\n", operationCount)
+}
+
+func BinarySearchAvg(arry []int) {
+	totalOperationCount := 0
+	start := time.Now()
+	for i := 0; i < len(arry); i++ {
+		_, operationCount := BinSearch(arry, i)
+		totalOperationCount += operationCount
+	}
+	TimeDiff(start, "Całkowity czas wykonania w przypadku średnim")
+	fmt.Printf("Średnia liczba operacji: %d\n", totalOperationCount)
+}
+
+func BinarySearchAvgTime(arry []int) {
+	iterations := 10
+	var elapsedTime, minTime, maxTime time.Duration
+
+	for i := 0; i < len(arry); i++ {
+		for j := 0; j < iterations+2; j++ {
+			start := time.Now()
+			_, _ = BinSearch(arry, arry[0])
+			iterationElapsed := time.Since(start)
+			elapsedTime += iterationElapsed
+
+			if iterationElapsed < minTime {
+				minTime = iterationElapsed
+			}
+			if iterationElapsed > maxTime {
+				maxTime = iterationElapsed
+			}
+		}
+	}
+
+	elapsedTime -= (minTime + maxTime)
+	fmt.Printf("Średni czas wynosi %f.\n", float64(elapsedTime.Microseconds())/float64(len(arry)))
 }
