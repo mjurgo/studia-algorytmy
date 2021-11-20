5a740bbc5f976e8dcc11b912f37745350a17d58e
Michal <mpjurgo@gmail.com>
2021-11-20 19:51:39 +0100

    Implement min, max and avg time measurement for binary search

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"algorytmy/proj1"
+	"time"
+	// "fmt"
 )
 
 func main() {
 	arryLen := proj1.Power(2, 25)
 	arry := proj1.PopulateArray(arryLen)
 
-	// proj1.BinarySearchMax(arry)
-	// proj1.BinarySearchMin(arry)
-	// proj1.BinarySearchAvg(arry)
-	proj1.BinarySearchAvgTime(arry)
+	proj1.BinSearchMinTime(arry)
+	proj1.BinSearchMaxTime(arry)
+	start := time.Now()
+	proj1.BinSearchAvgTime(arry)
+	proj1.TimeDiff(start, "Całość czasu wykonania funkcji dla średniego przypadku")
 }
--- proj1/binary_search.go
+++ proj1/binary_search.go
@@ -1,5 +1,10 @@
 package proj1
 
+import (
+	"fmt"
+	"time"
+)
+
 func BinSearch(arry []int, value int) (int, int) {
 	low := 0
 	high := len(arry) - 1
@@ -24,4 +29,98 @@ func BinSearch(arry []int, value int) (int, int) {
 	}
 
 	return -1, operationCounter
+}
+
+func BinSearchMinTime(arry []int) {
+	iterations := 10
+	operationsCount := 0
+	var elapsed time.Duration
+	var minTime, maxTime time.Duration
+	minTime = 99 * time.Hour
+
+	for i := 0; i < iterations+2; i++ {
+		start := time.Now()
+		_, operations := BinSearch(arry, len(arry)/2-1)
+
+		operationsCount += operations
+		iterationElapsed := time.Since(start)
+		elapsed += iterationElapsed
+		// fmt.Println(iterationElapsed)
+
+		if iterationElapsed < minTime {
+			minTime = iterationElapsed
+		}
+		if iterationElapsed > maxTime {
+			maxTime = iterationElapsed
+		}
+	}
+
+	elapsed -= (minTime + maxTime)
+	elapsed = time.Duration(int(elapsed.Nanoseconds()) / iterations)
+
+	fmt.Printf("Średni czas wykonania dla przypadku optymistycznego: %s\n", elapsed)
+	fmt.Printf("Liczba operacji w przypadku optymistycznym: %d\n", operationsCount/(iterations+2))
+}
+
+func BinSearchMaxTime(arry []int) {
+	iterations := 10
+	operationsCount := 0
+	var elapsed time.Duration
+	var minTime, maxTime time.Duration
+	minTime = 99 * time.Hour
+
+	for i := 0; i < iterations+2; i++ {
+		start := time.Now()
+		_, operations := BinSearch(arry, -5)
+
+		operationsCount += operations
+		iterationElapsed := time.Since(start)
+		elapsed += iterationElapsed
+		// fmt.Println(iterationElapsed)
+
+		if iterationElapsed < minTime {
+			minTime = iterationElapsed
+		}
+		if iterationElapsed > maxTime {
+			maxTime = iterationElapsed
+		}
+	}
+
+	elapsed -= (minTime + maxTime)
+	elapsed = time.Duration(int(elapsed.Nanoseconds()) / iterations)
+
+	fmt.Printf("Średni czas wykonania dla przypadku pesymistycznego: %s\n", elapsed)
+	fmt.Printf("Liczba operacji w przypadku pesymistycznym: %d\n", operationsCount/(iterations+2))
+}
+
+func BinSearchAvgTime(arry []int) {
+	iterations := 10
+	operationsCount := 0
+	var elapsed time.Duration
+	var minTime, maxTime time.Duration
+	minTime = 99 * time.Hour
+
+	for i := 0; i < len(arry); i++ {
+		for j := 0; j < iterations+2; j++ {
+			start := time.Now()
+			_, operations := BinSearch(arry, arry[i])
+
+			operationsCount += operations
+			iterationElapsed := time.Since(start)
+			elapsed += iterationElapsed
+
+			if iterationElapsed < minTime {
+				minTime = iterationElapsed
+			}
+			if iterationElapsed > maxTime {
+				maxTime = iterationElapsed
+			}
+		}
+	}
+
+	elapsed -= (minTime + maxTime)
+	elapsed = time.Duration(int(elapsed.Nanoseconds()) / iterations)
+
+	fmt.Printf("Średni czas wykonania: %s\n", elapsed)
+	fmt.Printf("Średnia liczba operacji: %d\n", operationsCount/len(arry)/(iterations+2))
 }
